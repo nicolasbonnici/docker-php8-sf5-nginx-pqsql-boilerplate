a8c43f64869ab370d2164e06164185435637845e
Nicolas Bonnici <nicolasbonnici@gmail.com>
2021-02-09 00:41:28 +0100

    Database container host fix for both local and container accessibility Symfony maker bundle added Various fixs and code lint

--- src/config/bundles.php
+++ src/config/bundles.php
@@ -8,4 +8,5 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
 ];
